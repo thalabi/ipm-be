a256583d87f97e093a4e24f71f21eaa9955a1f0a
Tarif Halabi <tarif.halabi@gmail.com>
2025-06-29 17:22:30 -0400

    Refactor.

--- ipm-be/csv-file-transfer-rest/src/main/java/com/kerneldc/ipm/rest/springconfig/SchedulingConfig.java
+++ ipm-be/csv-file-transfer-rest/src/main/java/com/kerneldc/ipm/rest/springconfig/SchedulingConfig.java
@@ -1,13 +1,13 @@
 package com.kerneldc.ipm.rest.springconfig;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
-import org.springframework.scheduling.config.ScheduledTaskRegistrar;
 
 @Configuration
 @EnableScheduling
-public class SchedulingConfig /*implements SchedulingConfigurer*/ {
+public class SchedulingConfig {
 
     private static final int POOL_SIZE = 10;
     private static final String THREAD_NAME_PREFIX = "scheduled-task-pool-";
@@ -17,15 +17,14 @@ public class SchedulingConfig /*implements SchedulingConfigurer*/ {
      *
      * @param taskRegistrar The task register.
      */
-//	@Override
-	public void configureTasks(ScheduledTaskRegistrar taskRegistrar) {
+	@Bean
+	public ThreadPoolTaskScheduler threadPoolTaskScheduler() {
 		ThreadPoolTaskScheduler threadPoolTaskScheduler = new ThreadPoolTaskScheduler();
 
 		threadPoolTaskScheduler.setPoolSize(POOL_SIZE);
 		threadPoolTaskScheduler.setThreadNamePrefix(THREAD_NAME_PREFIX);
-		threadPoolTaskScheduler.initialize();
-		
-		taskRegistrar.setTaskScheduler(threadPoolTaskScheduler);
-	}
+		threadPoolTaskScheduler.setWaitForTasksToCompleteOnShutdown(true); // Wait for tasks on shutdown
+        return threadPoolTaskScheduler;
+    }
 
 }

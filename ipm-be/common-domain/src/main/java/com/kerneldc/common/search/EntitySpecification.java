b7ef113b6f9fe6065ad4c413ec194a06f72114e4
Tarif Halabi <tarif.halabi@gmail.com>
2025-11-20 13:45:49 -0800

    Refactor.

--- ipm-be/common-domain/src/main/java/com/kerneldc/common/search/EntitySpecification.java
+++ ipm-be/common-domain/src/main/java/com/kerneldc/common/search/EntitySpecification.java
@@ -1,9 +1,12 @@
 package com.kerneldc.common.search;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 
 import org.apache.commons.lang3.StringUtils;
@@ -22,10 +25,14 @@ import lombok.extern.slf4j.Slf4j;
 public class EntitySpecification<T> implements Specification<T> {
 
 	private static final long serialVersionUID = 1L;
+	private final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
 
 	private enum QueryOperatorEnum {
-		EQUALS("equals"), NOT_EQUALS("notEquals"), GREATER_THAN("greaterThan"), GT("gt"),
-		GREATER_THAN_OR_EQUAL_TO("greaterThanOrEqualTo"), LESS_THAN("lessThan"), LT("lt"), LESS_THAN_OR_EQUAL_TO("lessThanOrEqualTo"),
+		EQUALS("equals"), NOT_EQUALS("notEquals"),
+		GREATER_THAN("greaterThan"), GT("gt"),
+		GREATER_THAN_OR_EQUAL_TO("greaterThanOrEqualTo"),  GTE("gte"),
+		LESS_THAN("lessThan"), LT("lt"),
+		LESS_THAN_OR_EQUAL_TO("lessThanOrEqualTo"), LTE("lte"),
 		STARTS_WITH("startsWith"), CONTAINS("contains"), NOT_CONTAINS("notContains"), ENDS_WITH("endsWith"),
 		DATE_IS("dateIs"), DATE_IS_NOT("dateIsNot"), DATE_BEFORE("dateBefore"), DATE_AFTER("dateAfter");
 
@@ -38,22 +45,22 @@ public class EntitySpecification<T> implements Specification<T> {
 		}
 		public static QueryOperatorEnum fromName(String name) {
 			for (QueryOperatorEnum e : QueryOperatorEnum.values()) {
-				if (e.getOperator().equals(name)) {
+				if (e.getOperator().equalsIgnoreCase(name)) {
 					return e;
 				}
 			}
-			return null;
+			throw new IllegalArgumentException(String.format("Invalid operator [%s]", name));
 		}
 	}
 	record Filter(String field, QueryOperatorEnum operator, String value) {};
 	private transient List<Filter> filterList = new ArrayList<>();
-	private transient EntityType<? extends AbstractEntity> entityMetamodel;
+	private transient EntityType<? extends AbstractEntity> entityMetaModel;
 
-	public EntitySpecification(EntityType<? extends AbstractEntity> entityMetamodel, String searchCriteria) {
+	public EntitySpecification(EntityType<? extends AbstractEntity> entityMetaModel, String searchCriteria) {
 		if (StringUtils.isEmpty(searchCriteria)) {
 			return;
 		}
-		this.entityMetamodel = entityMetamodel;
+		this.entityMetaModel = entityMetaModel;
 		filterList = Arrays.stream(searchCriteria.split(","))
 			    .map(criterion -> criterion.split("\\|"))
 			    .filter(parts -> parts.length == 3)
@@ -87,7 +94,7 @@ public class EntitySpecification<T> implements Specification<T> {
 		var field = inputFilter.field();
 		var value = inputFilter.value();
 		
-		var fieldType = entityMetamodel.getDeclaredAttribute(field).getJavaType().getSimpleName();
+		var fieldType = entityMetaModel.getDeclaredAttribute(field).getJavaType().getSimpleName();
 		LOGGER.info("fieldType: {}", fieldType);
 		LOGGER.info("input: {}, field: {}, value: {}", inputFilter, field, value);
 
@@ -95,43 +102,60 @@ public class EntitySpecification<T> implements Specification<T> {
 		case "String" -> {
 			return handleStringFieldType(inputFilter, field, value);
 		}
-		case "Double", "Float", "BigDecimal", "Short" -> {
+		case "Short", "Integer", "Double", "Float", "BigDecimal" -> {
 			return handleNumberFieldType(inputFilter, field, value);
 		}
 		case "LocalDateTime" -> {
 			return handleLocalDateTimeFieldType(inputFilter, field, value);
 		}
+		case "Date" -> {
+			return handleDateFieldType(inputFilter, field, value);
+		}
 		default -> {
 			var exceptionMessage = String.format("Field data type [%s], not supported.", fieldType);
-			LOGGER.error(exceptionMessage);
 			throw new IllegalArgumentException(exceptionMessage);
 		}
 		}
 		
 	}
-
-	private Specification<T> x(String field, String value) {
-		return (entity, query, criteriaBuilder) -> criteriaBuilder.equal(criteriaBuilder.lower(entity.get(field)), value.toLowerCase());
+	private Specification<T> handleLocalDateTimeFieldType(Filter inputFilter, String field, String value) {
+		var localDateTimeValue = LocalDateTime.parse(value, DateTimeFormatter.ISO_DATE_TIME);
+		switch (inputFilter.operator()) {
+		case DATE_IS -> {
+				return (entity, _, criteriaBuilder) -> criteriaBuilder.equal(entity.get(field), localDateTimeValue);
+		}
+		case DATE_IS_NOT -> {
+				return (entity, _, criteriaBuilder) -> criteriaBuilder.notEqual(entity.get(field), localDateTimeValue);
+		}
+		case DATE_BEFORE -> {
+			return (entity, _, criteriaBuilder) -> criteriaBuilder.lessThan(entity.get(field), localDateTimeValue);
+		}
+		case DATE_AFTER -> {
+			return (entity, _, criteriaBuilder) -> criteriaBuilder.greaterThan(entity.get(field), localDateTimeValue);
+		}
+		default -> throw new IllegalArgumentException("Unexpected value: " + inputFilter.operator());
+		}
 	}
+
 	private Specification<T> handleStringFieldType(Filter inputFilter, String field, String value) {
 		switch (inputFilter.operator()) {
 		case EQUALS -> {
-			return (entity, query, criteriaBuilder) -> criteriaBuilder.equal(criteriaBuilder.lower(entity.get(field)), value.toLowerCase());
+			return (entity, _, criteriaBuilder) -> criteriaBuilder.equal(criteriaBuilder.lower(entity.get(field)), value.toLowerCase());
 		}
 		case NOT_EQUALS -> {
-			return (entity, query, criteriaBuilder) -> criteriaBuilder.notEqual(criteriaBuilder.lower(entity.get(field)), value.toLowerCase());
+			return (entity, _, criteriaBuilder) -> criteriaBuilder.notEqual(criteriaBuilder.lower(entity.get(field)), value.toLowerCase());
 		}
 		case STARTS_WITH -> {
-			return (entity, query, criteriaBuilder) -> criteriaBuilder.like(criteriaBuilder.lower(entity.get(field)), value.toLowerCase() + "%");
+			return (entity, _, criteriaBuilder) -> criteriaBuilder.like(criteriaBuilder.lower(entity.get(field)), value.toLowerCase() + "%");
 		}
 		case CONTAINS -> {
-			return (entity, query, criteriaBuilder) -> criteriaBuilder.like(criteriaBuilder.lower(entity.get(field)), "%" + value.toLowerCase() + "%");
+			return (entity, _, criteriaBuilder) -> criteriaBuilder.like(criteriaBuilder.lower(entity.get(field)), "%" + value.toLowerCase() + "%");
 		}
 		case NOT_CONTAINS -> {
-			return (entity, query, criteriaBuilder) -> criteriaBuilder.not(criteriaBuilder.like(criteriaBuilder.lower(entity.get(field)), "%" + value.toLowerCase() + "%"));
+			return (entity, _, criteriaBuilder) -> criteriaBuilder.not(criteriaBuilder.like(criteriaBuilder.lower(entity.get(field)), "%" + value.toLowerCase() + "%"));
 		}
 		case ENDS_WITH -> {
-			return (entity, query, criteriaBuilder) -> criteriaBuilder.like(criteriaBuilder.lower(entity.get(field)), "%" + value.toLowerCase());
+			return (entity, _, criteriaBuilder) -> criteriaBuilder.like(criteriaBuilder.lower(entity.get(field)), "%" + value.toLowerCase());
 		}
 		default -> throw new IllegalArgumentException("Unexpected value: " + inputFilter.operator());
 		}
@@ -140,41 +164,46 @@ public class EntitySpecification<T> implements Specification<T> {
 	private Specification<T> handleNumberFieldType(Filter inputFilter, String field, String value) {
 		switch (inputFilter.operator()) {
 		case EQUALS -> {
-				return (entity, query, criteriaBuilder) -> criteriaBuilder.equal(entity.get(field), value);
+				return (entity, _, criteriaBuilder) -> criteriaBuilder.equal(entity.get(field), value);
 		}
 		case NOT_EQUALS -> {
-				return (entity, query, criteriaBuilder) -> criteriaBuilder.notEqual(entity.get(field), value);
+				return (entity, _, criteriaBuilder) -> criteriaBuilder.notEqual(entity.get(field), value);
 		}
 		case GREATER_THAN, GT -> {
-			return (entity, query, criteriaBuilder) -> criteriaBuilder.greaterThan(entity.get(field), value);
+			return (entity, _, criteriaBuilder) -> criteriaBuilder.greaterThan(entity.get(field), value);
 		}
-		case GREATER_THAN_OR_EQUAL_TO -> {
-			return (entity, query, criteriaBuilder) -> criteriaBuilder.greaterThanOrEqualTo(entity.get(field), value);
+		case GREATER_THAN_OR_EQUAL_TO, GTE -> {
+			return (entity, _, criteriaBuilder) -> criteriaBuilder.greaterThanOrEqualTo(entity.get(field), value);
 		}
 		case LESS_THAN, LT -> {
-			return (entity, query, criteriaBuilder) -> criteriaBuilder.lessThan(entity.get(field), value);
+			return (entity, _, criteriaBuilder) -> criteriaBuilder.lessThan(entity.get(field), value);
 		}
-		case LESS_THAN_OR_EQUAL_TO -> {
-			return (entity, query, criteriaBuilder) -> criteriaBuilder.lessThanOrEqualTo(entity.get(field), value);
+		case LESS_THAN_OR_EQUAL_TO, LTE -> {
+			return (entity, _, criteriaBuilder) -> criteriaBuilder.lessThanOrEqualTo(entity.get(field), value);
 		}
 		default -> throw new IllegalArgumentException("Unexpected value: " + inputFilter.operator());
 		}
 	}
 
-	private Specification<T> handleLocalDateTimeFieldType(Filter inputFilter, String field, String value) {
-		var localDateTimeValue = LocalDateTime.parse(value, DateTimeFormatter.ISO_DATE_TIME);
+	private Specification<T> handleDateFieldType(Filter inputFilter, String field, String value) {
+		Date date;
+		try {
+			date = dateFormat.parse(value);
+		} catch (ParseException e) {
+			throw new IllegalArgumentException("Unable to parse value: " + inputFilter.value(), e);
+		}
 		switch (inputFilter.operator()) {
 		case DATE_IS -> {
-				return (entity, query, criteriaBuilder) -> criteriaBuilder.equal(entity.get(field), localDateTimeValue);
+					return (entity, _, criteriaBuilder) -> criteriaBuilder.equal(entity.get(field), date);
 		}
 		case DATE_IS_NOT -> {
-				return (entity, query, criteriaBuilder) -> criteriaBuilder.notEqual(entity.get(field), localDateTimeValue);
+					return (entity, _, criteriaBuilder) -> criteriaBuilder.notEqual(entity.get(field), date);
 		}
 		case DATE_BEFORE -> {
-			return (entity, query, criteriaBuilder) -> criteriaBuilder.lessThan(entity.get(field), localDateTimeValue);
+				return (entity, _, criteriaBuilder) -> criteriaBuilder.lessThan(entity.get(field), date);
 		}
 		case DATE_AFTER -> {
-			return (entity, query, criteriaBuilder) -> criteriaBuilder.greaterThan(entity.get(field), localDateTimeValue);
+				return (entity, _, criteriaBuilder) -> criteriaBuilder.greaterThan(entity.get(field), date);
 		}
 		default -> throw new IllegalArgumentException("Unexpected value: " + inputFilter.operator());
 		}
